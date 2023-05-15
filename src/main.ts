b74d734a412a3d037b0ca3465b2258667b09d26a
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-05-15 22:27:08 +1000

    change the game to be a rotating box

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,32 @@
 import './style.css'
-import { Scene, Game, WEBGL, GameObjects } from 'phaser';
+import { Scene, Game, GameObjects } from 'phaser';
 
 const canvas = document.getElementById('game') as HTMLCanvasElement;
 
 class GameScene extends Scene {
-  private textbox: GameObjects.Text | undefined;
+  private box: GameObjects.Rectangle | undefined;
 
   constructor() {
     super('scene-game');
   }
 
   create() {
-    this.textbox = this.add.text(
-      window.innerWidth / 2,
-      window.innerHeight / 2,
-      'Welcome to Phaser x Vite!',
-      {
-        color: '#FFF',
-        fontFamily: 'monospace',
-        fontSize: '26px'
-      }
-    );
-
-    this.textbox.setOrigin(0.5, 0.5);
+    this.box = this.add.rectangle(200, 200, 100, 100, 0xff0000);
+    this.box.setStrokeStyle(4, 0x000000);
+    this.box.setOrigin(0.5, 0.5);
   }
 
   update(time: number, delta: number) {
-    if (!this.textbox) {
+    if (!this.box) {
       return;
     }
 
-    this.textbox.rotation += 0.0005 * delta;
+    this.box.rotation += 0.0005 * delta;
   }
 }
 
 const config = {
-  type: WEBGL,
+  type: Phaser.WEBGL,
   width: window.innerWidth,
   height: window.innerHeight,
   canvas,
