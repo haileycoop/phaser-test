e643bb1ac313b88b46fd7a9cd0e783ae0c79cb82
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-05-24 09:29:47 +1000

    update MyRoom.ts to progress multiplayer setup

--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -1,20 +1,25 @@
 import { Room, Client } from "@colyseus/core";
+import { type, MapSchema } from "@colyseus/schema";
 import { MyRoomState } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
+  @type({ map: "number" }) boxPosition: MapSchema<number> = new MapSchema<number>();
 
   onCreate (options: any) {
     this.setState(new MyRoomState());
 
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
+    this.onMessage("updateBoxPosition", (client, position) => {
+      this.boxPosition.set(client.sessionId, position);
     });
   }
 
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
+
+    // Send the initial box position to the joined client
+    if (this.boxPosition.size > 0) {
+      this.send(client, "updateBoxPosition", Array.from(this.boxPosition.entries()));
+    }
   }
 
   onLeave (client: Client, consented: boolean) {
