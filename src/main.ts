12f8b84061f742c937a9a2fce920d8b707716b76
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-06-03 15:41:58 +1000

    solved code execution order issue and box position now logs out on server

--- src/main.ts
+++ src/main.ts
@@ -12,41 +12,41 @@ const canvas = document.getElementById('game') as HTMLCanvasElement;
 let room: Colyseus.Room<unknown>;
 
 // Join or create a room
-client.joinOrCreate("game_room").then((roomInstance: Colyseus.Room<unknown>) => {
-    console.log(roomInstance.sessionId, "joined", roomInstance.name);
+// client.joinOrCreate("game_room").then((roomInstance: Colyseus.Room<unknown>) => {
+//     console.log(roomInstance.sessionId, "joined", roomInstance.name);
 
-    // Store the room instance in the variable
-    room = roomInstance;
+//     // Store the room instance in the variable
+//     room = roomInstance;
     
-    // Handle room events
-    // // Room state update
-    room.onStateChange((state) => {
-      console.log(room.name, "has new state:", state);
-    });
-    // Receive messages from server
-    room.onMessage("message_type", (message) => {
-      console.log(roomInstance.sessionId, "received on", room.name, message);
-    });
-    // Handle room errors
-    room.onError((code, message) => {
-      console.log(roomInstance.sessionId, "couldn't join", room.name);
-      console.error("Error:", message, " | Code: ", code); // Log the error message from the server
-    });
-    // Client left room
-    room.onLeave((code) => {
-      console.log(roomInstance.sessionId, "left", room.name, " | Code: ", code);
-    });
+//     // Handle room events
+//     // // Room state update
+//     room.onStateChange((state) => {
+//       console.log(room.name, "has new state:", state);
+//     });
+//     // Receive messages from server
+//     room.onMessage("message_type", (message) => {
+//       console.log(roomInstance.sessionId, "received on", room.name, message);
+//     });
+//     // Handle room errors
+//     room.onError((code, message) => {
+//       console.log(roomInstance.sessionId, "couldn't join", room.name);
+//       console.error("Error:", message, " | Code: ", code); // Log the error message from the server
+//     });
+//     // Client left room
+//     room.onLeave((code) => {
+//       console.log(roomInstance.sessionId, "left", room.name, " | Code: ", code);
+//     });
   
-    // Access the Colyseus room instance within the GameScene
-    const gameScene = new GameScene(roomInstance);
-    const config = {
-      // ... rest of the config
-      scene: [gameScene]
-    };
-    new Game(config);
-}).catch(e => {
-    console.log("JOIN ERROR", e);
-});
+//     // Access the Colyseus room instance within the GameScene
+//     const gameScene = new GameScene(roomInstance);
+//     const config = {
+//       // ... rest of the config
+//       scene: [gameScene]
+//     };
+//     new Game(config);
+// }).catch(e => {
+//     console.log("JOIN ERROR", e);
+// });
 
 
 class GameScene extends Scene {
@@ -59,24 +59,49 @@ class GameScene extends Scene {
   }
 
   create() {
+    // Create the box and make it interactive but not draggable yet
     this.box = this.add.rectangle(200, 200, 100, 100, 0xff0000);
     this.box.setStrokeStyle(4, 0x000000);
     this.box.setOrigin(0.5, 0.5);
     this.box.setInteractive();
-    this.input.setDraggable(this.box, true);
 
-    // Logging a message using the room instance
-    if (this.room) {
-      console.log("Connected to room:", this.room.name);
-    }
-
-    // Bind the handleDrag function to the correct context
-    this.handleDrag = this.handleDrag.bind(this);
-
-    // Add the drag event listener
-    this.input.on('drag', this.handleDrag);
-
+    // Start connecting to the room
+    client.joinOrCreate("game_room").then((roomInstance: Colyseus.Room<unknown>) => {
+        this.room = roomInstance;
+        // Make the box draggable now that the room is ready
+        if (this.box) {
+            this.input.setDraggable(this.box, true);
+        }
+        // Bind the handleDrag function to the correct context
+        this.handleDrag = this.handleDrag.bind(this);
+        // Add the drag event listener
+        this.input.on('drag', this.handleDrag);
+    }).catch(e => {
+        console.log("JOIN ERROR", e);
+    });
   }
+
+  // create() {
+  //   this.box = this.add.rectangle(200, 200, 100, 100, 0xff0000);
+  //   this.box.setStrokeStyle(4, 0x000000);
+  //   this.box.setOrigin(0.5, 0.5);
+  //   this.box.setInteractive();
+  //   this.input.setDraggable(this.box, true);
+
+  //   // Logging a message using the room instance
+  //   if (this.room) {
+  //     console.log("this.room:", this.room);
+  //     console.log("Connected to room:", this.room.name);
+  //   }
+
+  //   // Bind the handleDrag function to the correct context
+  //   this.handleDrag = this.handleDrag.bind(this);
+
+  //   // Add the drag event listener
+  //   this.input.on('drag', this.handleDrag);
+
+  // }
+
 
   handleDrag(pointer: Phaser.Input.Pointer, gameObject: GameObjects.GameObject, dragX: number, dragY: number) {
     if (gameObject instanceof Phaser.GameObjects.Rectangle) {
